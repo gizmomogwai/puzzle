e975af1b7167c703e3e0c144b9df70a488f1c040
Christian Köstlin <christian.koestlin@esrlabs.com>
2017-03-10 10:22:24 +0100

    fixed rakefile to create out if not already there

--- rakefile.rb
+++ rakefile.rb
@@ -9,19 +9,26 @@ scramble_all = task :scramble_all
 desc 'unscramble_all'
 unscramble_all = task :unscramble_all
 
+directory "out"
+
 Dir.glob("data/*_medium.*").each do |input|
   scrambled_output = "out/#{File.basename(input)}.scrambled.png"
   unscrambled_output = "#{scrambled_output}.unscrambled.png"
 
   desc scrambled_output
-  t = task scrambled_output do
+  t = task scrambled_output => "out" do
     sh "./puzzle scramble #{input}"
   end
   scramble_all.enhance([t])
 
   desc unscrambled_output
-  t = task unscrambled_output do
-    sh "./puzzle unscramble #{scrambled_output}"
+  t = task unscrambled_output => "out" do
+    start = Time.now
+    sh "gtime --verbose ./puzzle unscramble #{scrambled_output}"
+    puts "took #{Time.now - start}"
+    #start = Time.now
+    #sh "gtime --verbose ./puzzle unscrambleP #{scrambled_output}"
+    #puts "took #{Time.now - start}"
   end
   unscramble_all.enhance([t])
 end
--- source/app.d
+++ source/app.d
@@ -1,12 +1,14 @@
 // see discussion https://news.ycombinator.com/item?id=12667611
 
 import std.stdio;
+import std.datetime;
 import std.string;
 import std.random;
 import std.math;
 import imageformats;
 import std.conv;
 import std.path;
+import std.algorithm;
 
 void swapCols(IFImage* image, int colA, int colB) {
   for (int j=0; j<image.h; ++j) {
@@ -21,32 +23,26 @@ void swapCols(IFImage* image, int colA, int colB) {
   }
 }
 
-int calcDelta(IFImage* image, int colA, int colB, int max) {
-  int colAStart = colA * 3;
-  int colBStart = colB * 3;
-  int sum = 0;
-  for (int y=0; y<image.h; y++) {
-    int offset = y*image.w * 3;
+ulong calcDelta(IFImage* image, ulong colA, ulong colB) {
+  auto colAStart = colA * 3;
+  auto colBStart = colB * 3;
+  ulong sum = 0;
+  for (ulong y=0; y<image.h; y++) {
+    auto offset = y*image.w * 3;
 
-    int offsetA = offset + colAStart;
-    int offsetB = offset + colBStart;
+    ulong offsetA = offset + colAStart;
+    ulong offsetB = offset + colBStart;
     for (int i=0; i<3; ++i) {
-      sum += abs(image.pixels[offsetA + i] - image.pixels[offsetB + i]);
+      auto delta = abs(image.pixels[offsetA + i] - image.pixels[offsetB + i]);
+      sum += delta;
     }
-    /*
-      // does not work with sqrt
-    if (sum > max) {
-      return max;
-    }
-    */
   }
-  return cast(int)sqrt(cast(float)sum);
-  // return sum;
+  return sum;
 }
 
 unittest {
   const w = 2;
-  const h = 2;
+  const h = 1;
   const cf = ColFmt.RGB;
   auto i = IFImage(w, h, cf, new ubyte[w*h*cf]);
   i.pixels[0] = 0;
@@ -57,27 +53,114 @@ unittest {
   i.pixels[4] = 0;
   i.pixels[5] = 4;
 
-  auto d = calcDelta(&i, 0, 1, 10);
-  assert(36 == d);
-
-  d = calcDelta(&i, 0, 1, 4);
-  assert(16 == d);
+  auto d = calcDelta(&i, 0, 1);
+  assert(6 == d);
 }
 
 struct Match {
-  int column;
-  int index;
-  int delta;
+  ulong column;
+  ulong index;
+  ulong delta;
+}
+import std.parallelism;
+
+struct Work {
+  IFImage* image;
+  long forColumn;
+  ulong[] todo;
+  ulong fromIndex;
+  ulong toIndex;
+
+  ulong column = 0;
+  ulong index = int.max;
+  ulong delta = 0;
+  Work calc() {
+    delta = int.max;
+
+    auto colAStart = forColumn * 3;
+    for (auto currentIndex=fromIndex; currentIndex<toIndex; ++currentIndex) {
+
+      auto currentColumn = todo[currentIndex];
+      ulong currentDelta = 0;
+      auto colBStart = currentColumn * 3;
+      for (int y=0; y<image.h; y++) {
+        auto offset = y*image.w * 3;
+
+        auto offsetA = offset + colAStart;
+        auto offsetB = offset + colBStart;
+        for (int i=0; i<3; ++i) {
+          currentDelta += abs(image.pixels[offsetA + i] - image.pixels[offsetB + i]);
+        }
+      }
+      
+      if (currentDelta < delta) {
+        delta = currentDelta;
+        column = currentColumn;
+        index = currentIndex;
+      }
+    }
+    return this;
+  }
 }
 
-Match findBest(int forColumn, IFImage* image, ref int[] todo) {
-  int delta = int.max;
-  int column = -1;
-  int index = -1;
+Work findBestP(ulong forColumn, IFImage* image, ref ulong[] todo) {
+  Work[] work;
+  for (int i=0; i<todo.length; i+=100) {
+    work ~= Work(image, forColumn, todo, i, min(i+100, todo.length));
+  }
+  auto deltas = taskPool.map!("a.calc")(work);
+  Work w;
+  w.delta = int.max;
+  foreach (d; deltas) {
+    if (d.delta < w.delta) {
+      w = d;
+    }
+  }
+  return w;
+}
 
-  int i = 0;
+unittest {
+  const w = 2000;
+  const h = 2000;
+  const cf = ColFmt.RGB;
+  auto image = IFImage(w, h, cf, new ubyte[w*h*cf]);
+  writeln("test");
+  Work work = Work(&image, 0, 1, 10);
+  auto mt = measureTime!((TickDuration a)
+                         { writeln(a.to!("msecs", float)); });
+  for (int i=0; i<100; ++i) {
+    auto delta = work.calc();
+  }
+}
+
+unittest {
+  const w = 2;
+  const h = 2;
+  const cf = ColFmt.RGB;
+  auto image = IFImage(w, h, cf, new ubyte[w*h*cf]);
+  image.pixels[0] = 0;
+  image.pixels[1] = 2;
+  image.pixels[2] = 0;
+
+  image.pixels[3] = 0;
+  image.pixels[4] = 0;
+  image.pixels[5] = 4;
+
+  Work work = Work(&image, 0, 1, 10);
+  auto delta = work.calc();
+  assert(delta.delta == 6);
+  assert(delta.index == 10);
+}
+
+Match findBest(ulong forColumn, IFImage* image, ref ulong[] todo) {
+  auto delta = ulong.max;
+  ulong column = -1;
+  ulong index = -1;
+
+  ulong i = 0;
+
   foreach (c; todo) {
-    auto d = calcDelta(image, forColumn, c, delta);
+    auto d = calcDelta(image, forColumn, c);
     if (d < delta) {
       delta = d;
       column = c;
@@ -88,7 +171,7 @@ Match findBest(int forColumn, IFImage* image, ref int[] todo) {
   return Match(column, index, delta);
 }
 
-int[] remove(int[] data, int idx) {
+ulong[] remove(ulong[] data, ulong idx) {
   if (idx < 0) {
     throw new Exception("out of bounds: " ~ idx.to!string ~ " < " ~ data.length.to!string);
   }
@@ -101,20 +184,20 @@ int[] remove(int[] data, int idx) {
   return data[0..idx] ~ data[idx+1..$];
 }
 
-void copyColumn(IFImage* from, int sourceIndex, IFImage* to, int targetIndex) {
-  for (int j=0; j<from.h; j++) {
-    int sourceOffset = (j*from.w + sourceIndex) * from.c;
-    int targetOffset = (j*from.w + targetIndex) * from.c;
+void copyColumn(IFImage* from, ulong sourceIndex, IFImage* to, ulong targetIndex) {
+  for (auto j=0; j<from.h; j++) {
+    auto sourceOffset = (j*from.w + sourceIndex) * from.c;
+    auto targetOffset = (j*from.w + targetIndex) * from.c;
     to.pixels[targetOffset] = from.pixels[sourceOffset];
     to.pixels[targetOffset+1] = from.pixels[sourceOffset+1];
     to.pixels[targetOffset+2] = from.pixels[sourceOffset+2];
   }
 }
 
-IFImage* reassembleWith(IFImage image, int[] order) {
+IFImage* reassembleWith(IFImage image, ulong[] order) {
   auto pixels = new ubyte[image.pixels.length];
   auto res = new IFImage(image.w, image.h, image.c, pixels);
-  int targetIndex = 0;
+  ulong targetIndex = 0;
   foreach (sourceIndex; order) {
     copyColumn(&image, sourceIndex, res, targetIndex);
     targetIndex++;
@@ -123,16 +206,50 @@ IFImage* reassembleWith(IFImage image, int[] order) {
 }
 
 IFImage* unscramble(IFImage image) {
-  auto todo = new int[image.w-1];
+  auto todo = new ulong[image.w-1];
   for (int i=1; i<image.w; i++) {
     todo[i-1] = i;
   }
 
-  int[] result;
+  ulong[] result;
   result ~= 0;
 
   while (todo.length > 0) {
-    // writeln("todo: ", todo.length);
+    writeln(todo.length);
+    auto leftBest = findBest(result[0], &image, todo);
+    auto rightBest = findBest(result[$-1], &image, todo);
+    if (leftBest.delta < rightBest.delta) {
+      result = [leftBest.column] ~ result;
+      todo = remove(todo, leftBest.index);
+    } else {
+      result = result ~ rightBest.column;
+      todo = remove(todo, rightBest.index);
+    }
+  }
+  return image.reassembleWith(result);
+}
+
+IFImage* unscrambleP(IFImage image) {
+  auto todo = new ulong[image.w-1];
+  for (ulong i=1; i<image.w; i++) {
+    todo[i-1] = i;
+  }
+
+  ulong[] result;
+  result ~= 0;
+  while (todo.length > 100) {
+    writeln(todo.length);
+    auto leftBest = findBestP(result[0], &image, todo);
+    auto rightBest = findBestP(result[$-1], &image, todo);
+    if (leftBest.delta < rightBest.delta) {
+      result = [leftBest.column] ~ result;
+      todo = remove(todo, leftBest.index);
+    } else {
+      result = result ~ rightBest.column;
+      todo = remove(todo, rightBest.index);
+    }
+  }
+  while (todo.length > 0) {
     auto leftBest = findBest(result[0], &image, todo);
     auto rightBest = findBest(result[$-1], &image, todo);
     if (leftBest.delta < rightBest.delta) {
@@ -147,9 +264,9 @@ IFImage* unscramble(IFImage image) {
 }
 
 int main(string[] args) {
-  writeln(args);
+  writeln(args.join(" "));
   if (args.length != 3) {
-    writeln("Usage: %s scramble|unscramble input (outputname is derived from inputname)".format(args[0]));
+    writeln("Usage: %s scramble|unscramble|unscrambleP input (outputname is derived from inputname)".format(args[0]));
     return 1;
   }
   auto action = args[1];
@@ -168,6 +285,9 @@ int main(string[] args) {
     write_png(outputFileName, input.w, input.h, input.pixels);
   } else if (action == "unscramble") {
     auto output = input.unscramble();
+    write_png(outputFileName, output.w, output.h, output.pixels);
+  } else if (action == "unscrambleP") {
+    auto output = input.unscrambleP();
     write_png(outputFileName, output.w, output.h, output.pixels);
   }
   return 0;
